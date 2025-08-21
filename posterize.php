400a59ae66033334d344b725fc74994702d5e32a
Oliver Kurmis <oliver@kurmis.com>
2025-08-21 23:42:47 +0200

    Webpage to make poster from a PDF

---
+++ posterize.php
@@ -0,0 +1,28 @@
+<?php
+if ($_FILES["if"]["type"] != "application/pdf") {
+  exit("Sorry, only PDF files are allowed.");
+}
+if ($_FILES["if"]["size"] > 1024*1024*10) {
+  exit("Sorry, maximum allowed file size is 10MB.");
+}
+
+$tmpPoster = $_FILES["if"]["tmp_name"].'_poster';
+try {
+  $result = system('pdfposter -p 2x2a4 ' . $_FILES["if"]["tmp_name"] . ' ' . $tmpPoster, $resultCode);
+} catch (Exception $e) {
+  exit('Sorry, there was an error processing your file: ' . $e->getMessage());
+} finally {
+  unlink($_FILES["if"]["tmp_name"]);
+}
+if (!file_exists($tmpPoster)) {
+  exit("Sorry, something went wrong.");
+}
+
+header('Content-Description: File Transfer');
+header('Content-Type: application/pdf');
+header('Content-Disposition: attachment; filename="Poster_'.$_FILES["if"]["name"].'"');
+header('Expires: 0');
+header('Cache-Control: private');
+header('Content-Length: ' . filesize($tmpPoster));
+readfile($tmpPoster);
+unlink($tmpPoster);
